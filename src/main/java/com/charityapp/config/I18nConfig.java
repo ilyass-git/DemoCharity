300a0a514b5defa03fbd03ae3e4d0a75817fb78b
ilyass <ilyasmachmoum@gmail.com>
2025-05-23 10:51:35 +0100

    debut d'internationalisation

---
+++ src/main/java/com/charityapp/config/I18nConfig.java
@@ -0,0 +1,35 @@
+package com.charityapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class I18nConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver slr = new CookieLocaleResolver();
+        slr.setDefaultLocale(Locale.FRENCH);
+        slr.setCookieName("LOCALE");
+        slr.setCookieMaxAge(60 * 60 * 24 * 30); // 30 jours
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+} 
